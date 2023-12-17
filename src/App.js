17a0594af042287e4bb7d621a23f162364fc4676
Gurunanthan <gurunanthan.r.j@gmail.com>
2023-12-17 12:49:18 +0530

    added the styling and fetching user details to display the user

--- src/App.js
+++ src/App.js
@@ -1,45 +1,111 @@
-import { useState }  from 'react'
-import { createUserWithEmailAndPassword , signInWithPopup , signOut} from 'firebase/auth'
-import { auth ,gooleAuth } from './firebase.utils.js'
+import React, { useEffect, useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { auth, gooleAuth } from './firebase.utils.js';
 
 const Auth = () => {
-  const[email , setEmail] = useState("")
-  const[password, setPassword] = useState("")
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
 
-  console.log(auth?.currentUser?.photoURL);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
 
-  const handleSignIn = async () =>{
-    try{
-    await createUserWithEmailAndPassword(auth , email , password)
-  } catch(err){
-    console.error(err);
-  }}
+    return () => unsubscribe();
+  }, []);
 
-  const handleGoogleSignIn = async () =>{
-    try{
-    await signInWithPopup(auth , gooleAuth)
-  } catch(err){
-    console.error(err);
-  }}
+  const handleSignIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-  const handleSignOut = async () =>{
-    try{
-    await signOut(auth)
-  } catch(err){
-    console.error(err);
-  }}
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithPopup(auth, gooleAuth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
-    <div>
-        <input type='email' placeholder='email....' onChange={e => setEmail(e.target.value)}/>
-        <input type='password'  placeholder='password...' onChange={e => setPassword(e.target.value)}/>
-        <button onClick={handleSignIn}>Sign in</button>
-        <button onClick={handleGoogleSignIn}>Sign In With Google</button>
-        <button onClick={handleSignOut}>Log Out</button>
-
-
+    <div style={styles.container}>
+      <h2>Authentication</h2>
+      {currentUser ? (
+        <div>
+          <p>Welcome, {currentUser.email}!</p>
+          <button style={styles.button} onClick={handleSignOut}>
+            Log Out
+          </button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={styles.input}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={styles.input}
+          />
+          <button style={styles.button} onClick={handleSignIn}>
+            Sign In
+          </button>
+          <button style={styles.button} onClick={handleGoogleSignIn}>
+            Sign In With Google
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Auth
+const styles = {
+  container: {
+    maxWidth: '400px',
+    margin: 'auto',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '8px',
+    textAlign: 'center',
+  },
+  input: {
+    width: '100%',
+    padding: '10px',
+    margin: '5px 0',
+    boxSizing: 'border-box',
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    padding: '10px 20px',
+    margin: '5px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  },
+};
+
+export default Auth;
